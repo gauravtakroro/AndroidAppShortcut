36fb6832ed5c356d29c4ccca1175759e44625cf3
Gaurav Tak <gaurav.tak@roro.io>
2023-08-01 23:36:38 +0530

    Shortcuts added with programming way

---
+++ app/src/main/java/io/gauravtak/androidappshortcut/Constants.kt
@@ -0,0 +1,6 @@
+package io.gauravtak.androidappshortcut
+
+object Constants {
+    val YOUTUBE_URL = "https://www.youtube.com/"
+    val LINKEDIN_URL= "https://www.linkedin.com/"
+}
--- app/src/main/java/io/gauravtak/androidappshortcut/MainActivity.kt
+++ app/src/main/java/io/gauravtak/androidappshortcut/MainActivity.kt
@@ -1,11 +1,46 @@
 package io.gauravtak.androidappshortcut
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addShortCutsUsingCode()
+    }
+
+    private fun addShortCutsUsingCode() {
+
+        val shortcutManager = getSystemService<ShortcutManager>(ShortcutManager::class.java)
+
+        val shortcut = ShortcutInfo.Builder(applicationContext, "id_linked_in")
+            .setShortLabel(getString(R.string.linkedin))
+            .setLongLabel(getString(R.string.open_linkedin))
+            .setIcon(Icon.createWithResource(applicationContext, R.drawable.ic_linkedin))
+            .setIntent(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(Constants.LINKEDIN_URL))
+            )
+            .build()
+
+        val shortcut2 = ShortcutInfo.Builder(applicationContext, "id_youtube")
+            .setShortLabel(getString(R.string.youtube))
+            .setLongLabel(getString(R.string.open_youtube))
+            .setIcon(Icon.createWithResource(applicationContext, R.drawable.ic_youtube))
+            .setIntent(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(Constants.YOUTUBE_URL))
+            )
+            .build()
+        shortcutManager!!.dynamicShortcuts = listOf(shortcut, shortcut2)
     }
 }
