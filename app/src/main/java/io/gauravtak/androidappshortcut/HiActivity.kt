b8a3e3ad60ccf4a506d49086f67b29fd9800314f
Gaurav Tak <gaurav.tak@roro.io>
2023-08-01 23:32:52 +0530

    Hi Activity Added with the flow

---
+++ app/src/main/java/io/gauravtak/androidappshortcut/HiActivity.kt
@@ -0,0 +1,11 @@
+package io.gauravtak.androidappshortcut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hi)
+    }
+}
